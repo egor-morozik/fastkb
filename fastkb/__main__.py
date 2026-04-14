32cc52464accb6169ad8a02fd8507cbb7d7ecbfa
Egor Morozik <egor.mrz04@gmail.com>
2026-04-14 13:34:22 +0300

    feature: init uv & create init command to CLI (create sqlite db and add table to db)

--- fastkb/__main__.py
+++ fastkb/__main__.py
@@ -1,5 +1,56 @@
+import sqlite3
+
+import argparse
+
+import sys
+
+
+def init_db():
+    """
+    Create database fastkb.db and documents table.
+    """
+
+    try:
+        connection = sqlite3.connect("fastkb.db")
+        cursor = connection.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS documents (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                content TEXT,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        """)
+        connection.commit()
+        connection.close()
+        print("Database fastkb.db init, table 'documents' created.")
+    except Exception as e:
+        print(f"Init database error: {e}")
+        sys.exit(1)
+
+
 def main():
-    print("start")
+    parser = argparse.ArgumentParser(
+        prog="fastkb",
+        description="FastKB CLI tool",
+    )
+
+    subparsers = parser.add_subparsers(
+        dest="command",
+        help="All commands",
+    )
+    subparsers.add_parser(
+        "init",
+        help="Init database",
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "init":
+        init_db()
+    else:
+        parser.print_help()
+
 
 if __name__ == "__main__":
     main()
